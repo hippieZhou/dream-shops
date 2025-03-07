6dec8f9c86472f941d45142b129b37a5159468c4
qiang zhou <qzhou@ritchiebros.com>
2025-03-07 21:35:38 +0800

    feat: creating and mapping entity classes

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## Database setup
+
+```bash
+docker run -itd --name mysql-test -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 mysql
+```
+
+## Docs
+
+- [Spring Boot, Spring Security, JWT Course – Shopping Cart Backend Java Project](https://www.youtube.com/watch?v=oGhc5Z-WJSw&ab_channel=freeCodeCamp.org)
---
+++ src/main/java/com/hippiezhou/dreamshops/model/Product.java
@@ -0,0 +1,33 @@
+package com.hippiezhou.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+}
